5c771aa896a08c43de653acde59856bb8aa63bff
cg-c <52759655+cg-c@users.noreply.github.com>
2025-07-24 01:15:39 -0400

    1.4: Course Information step 4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -15,29 +15,25 @@ const Part = (prop) => {
 }
 
 const Content = (parts) => {
-  // let all = []
-
-  // for (const item of parts.parts) {
-  //   all.push(<p>{item.name} {item.num}</p>)
-  // }
-
-  // return (
-  //     <>
-  //       {all}
-  //     </>
-  //   )
-
   return (
     <>
-      <Part part={parts.parts} />
+      <Part part={parts.parts[0]} />
+      <Part part={parts.parts[1]} />
+      <Part part={parts.parts[2]} />
     </>
   )
 }
 
-const Total = (total) => {
+const Total = (parts) => {
+  let total = 0
+
+  parts.parts.forEach(val => {
+    total += val.exercises
+  })
+
   return (
     <>
-      <p>Number of exercises {total.total}</p>
+      <p>Number of exercises {total}</p>
     </>
   )
 }
@@ -50,26 +46,26 @@ const App = () => {
   //   {name: 'Using props to pass data', num: 7}, 
   //   {name: 'State of a component', num: 14},
   // ]
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={part1} />
-      <Content parts={part2} />
-      <Content parts={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
