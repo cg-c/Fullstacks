3aede2bc565144a6029a96a3f0d1934dd270a6c0
cg-c <52759655+cg-c@users.noreply.github.com>
2025-07-24 00:35:29 -0400

    1.1: Course Information

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Header = (course) => {
+  return (
+    <>
+      <h1>{course.course}</h1>
+    </> 
+  )
+}
+
+const Content = (parts) => {
+  let all = []
+
+  for (const item of parts.parts) {
+    all.push(<p>{item.name} {item.num}</p>)
+  }
+
+  return (
+      <>
+        {all}
+      </>
+    )
+}
+
+const Total = (parts) => {
+  let total = 0
+
+  for (const i of parts.parts) {
+    total += i.num
+  }
+
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {name: 'Fundamentals of React', num: 10},
+    {name: 'Using props to pass data', num: 7}, 
+    {name: 'State of a component', num: 14},
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
