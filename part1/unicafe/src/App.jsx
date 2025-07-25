e51e3bbab4b0c4807f84a7e3ec8fd3edd1e9d980
cg-c <52759655+cg-c@users.noreply.github.com>
2025-07-24 23:07:29 -0400

    1.8: unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -26,21 +26,17 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
-    setAll(all + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
-    setAll(all + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
-    setAll(all + 1)
   }
 
   return (
