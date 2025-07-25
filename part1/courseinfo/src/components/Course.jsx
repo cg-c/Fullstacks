afe8d6d0592969d408cc2a7bea1866ca16e7fb38
cg-c <52759655+cg-c@users.noreply.github.com>
2025-07-25 03:59:56 -0400

    2.4: Course information step 9

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -1,15 +1,32 @@
-const Course = ({ course }) => {
+const Header = ({name}) => <h2>{name}</h2>
+
+const Part = ({name, exercises}) => <p>{name} {exercises}</p>
+
+
+const Course = ({ courses }) => {
+
+    const getAllParts = (parts) => {
+        return (
+            <>
+                {parts.map(part =>
+                    <Part key={part.id} name={part.name} exercises={part.exercises} />
+                )}
+            </>
+        )
+    }
 
     return (
         <>
-            <h1>{course.name}</h1>
-            {course.parts.map(part => 
-                <p key={part.id}>{part.name} {part.exercises}</p>
+            <h1>Web development curriculum </h1>
+            {courses.map(course => 
+                <div key={course.id}>
+                    <Header name={course.name}/>
+                    {getAllParts(course.parts)}
+                    <p><b>total of {course.parts.reduce((sum, val) => {return sum + val.exercises}, 0)} exercises</b></p>
+                </div>
             )}
-            <p><b>total of {course.parts.reduce((sum, val) => {return sum + val.exercises}, 0)} exercises</b></p>
         </>
     )
-
 }
 
 export default Course
