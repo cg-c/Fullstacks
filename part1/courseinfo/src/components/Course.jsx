6853a04df980217f0768ac291aa80455582df56c
cg-c <52759655+cg-c@users.noreply.github.com>
2025-07-25 03:14:02 -0400

    2.2: Course information step 7

--- part1/courseinfo/src/components/Course.jsx
+++ part1/courseinfo/src/components/Course.jsx
@@ -3,7 +3,10 @@ const Course = ({ course }) => {
     return (
         <>
             <h1>{course.name}</h1>
-            {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+            {course.parts.map(part => 
+                <p key={part.id}>{part.name} {part.exercises}</p>
+            )}
+            <p><b>total of {course.parts.reduce((sum, val) => {return sum + val.exercises}, 0)} exercises</b></p>
         </>
     )
 
