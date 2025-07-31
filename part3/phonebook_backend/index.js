414852681cb4ad36efdf3290cfd4234c69cbe0c1
cg-c <52759655+cg-c@users.noreply.github.com>
2025-07-31 02:08:17 -0400

    3.5: Phonebook backend step 5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -61,6 +61,40 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+  let randID = Math.ceil(Math.random() * (10000 - 1))
+  
+  while (persons.find(p => p.id == randID)) {
+    randID = Math.ceil(Math.random() * (10000 - 1))
+  }
+  return String(randID)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    else if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    console.log(person.id)
+
+    persons.concat(person)
+    response.json(persons)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
