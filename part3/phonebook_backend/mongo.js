6361b497cdeb52bc059d5440658b99cf5d683b62
cg-c <52759655+cg-c@users.noreply.github.com>
2025-08-01 06:24:31 -0400

    3.12: Command-line database

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -21,8 +21,9 @@ const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
     Person.find({}).then(result => {
+        console.log("phonebook:")
         result.forEach(p => {
-            console.log(p)
+            console.log(`${p.name} ${p.number}`)
         })
         mongoose.connection.close()
     })
