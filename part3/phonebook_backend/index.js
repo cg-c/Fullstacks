97902b718e96b1e7fe4688f30ffa2930840cb618
cg-c <52759655+cg-c@users.noreply.github.com>
2025-08-02 04:10:27 -0400

    3.15: Phonebook database, step 3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -3,8 +3,6 @@ const express = require('express')
 const morgan = require('morgan')
 const Person = require('./models/person')
 
-const app = express()
-
 let persons = [
     { 
       "id": "1",
@@ -28,6 +26,8 @@ let persons = [
     }
 ]
 
+const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 
 morgan.token('data', (request, response) => {
@@ -37,7 +37,6 @@ morgan.token('data', (request, response) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
-app.use(express.static('dist'))
 
 
 app.get('/', (request, response) => {
@@ -65,10 +64,11 @@ app.get('/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 // const generateId = () => {
@@ -119,6 +119,18 @@ app.post('/api/persons', (request, response) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
