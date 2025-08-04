b2f233e389d4adbf4f86e7354a771c5e40e029c5
cg-c <52759655+cg-c@users.noreply.github.com>
2025-08-03 23:13:51 -0400

    4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
+const blog = require("../models/blog")
+
 const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, b) => {
+    return sum + b.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
