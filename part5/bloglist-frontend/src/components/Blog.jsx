da416e09fa680940ea86d116542382f5b0ac8159
cg-c <52759655+cg-c@users.noreply.github.com>
2025-08-13 04:32:59 -0400

    5.7 Blog List Frontend, step 7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,40 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const hideWhenVisible = { display: view ? 'none' : '' }
+  const showWhenVisible = { display: view ? '' : 'none' }
+
+  const toggleView = () => {
+    setView(!view)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleView}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleView}>hide</button>
+        <br/>
+        {blog.url}<br/>
+        {blog.likes} <button>like</button>
+        <br/>
+        {blog.user.name}
+      </div>
+  </div>
+)}
+
 
 export default Blog
